d31e7cb48bc91f47309d58c5c6b5513620debf7f
wenqiangperth <2280064372@qq.com>
2018-12-17 19:29:53 +0800

    部分controller、service架子写了，嗯，还有一些写的时候看着改些

--- user/src/main/java/com/example/user/service/RequestService.java
+++ user/src/main/java/com/example/user/service/RequestService.java
@@ -3,7 +3,7 @@ package com.example.user.service;
 /**
  * @author perth
  * @ClassName RequestService
- * @Description TODO
+ * @Description teacher处理各类请求
  * @Date 2018/12/17 19:08
  * @Version 1.0
  **/
