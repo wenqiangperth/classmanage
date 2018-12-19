93e2df00e137c5d8cfae91d8b36215eb158fa0af
wenqiangperth <2280064372@qq.com>
2018-12-19 22:37:06 +0800

    这是组队的规则验证

---
+++ common/src/main/java/com/example/common/entity/MemberLimitStrategy.java
@@ -0,0 +1,18 @@
+package com.example.common.entity;
+
+import lombok.Data;
+
+/**
+ * @author perth
+ * @ClassName MemberLimitStrategy
+ * @Description TODO
+ * @Date 2018/12/19 20:47
+ * @Version 1.0
+ **/
+@Data
+public class MemberLimitStrategy {
+    private Long id;
+    private int minMember;
+    private int maxMember;
+    private Long courseId;
+}
---
+++ common/src/main/java/com/example/common/entity/TeamAndOrStrategy.java
@@ -0,0 +1,19 @@
+package com.example.common.entity;
+
+import lombok.Data;
+
+/**
+ * @author perth
+ * @ClassName TeamAndOrStrategy
+ * @Description TODO
+ * @Date 2018/12/19 21:17
+ * @Version 1.0
+ **/
+@Data
+public class TeamAndOrStrategy {
+    private Long id;
+    private String strategyName1;
+    private Long strategyId1;
+    private String strategyName2;
+    private Long strategyId2;
+}
