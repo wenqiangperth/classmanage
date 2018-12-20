a4fd1f9071545eff15081307e5d41360adbfd2f3
wenqiangperth <2280064372@qq.com>
2018-12-20 09:12:42 +0800

    这是组队的规则验证

---
+++ common/src/main/java/com/example/common/entity/ConflictCourseStrstegy.java
@@ -0,0 +1,17 @@
+package com.example.common.entity;
+
+import lombok.Data;
+
+/**
+ * @author perth
+ * @ClassName ConflictCourseStrstegy
+ * @Description TODO
+ * @Date 2018/12/20 8:58
+ * @Version 1.0
+ **/
+@Data
+public class ConflictCourseStrstegy {
+    private Long id;
+    private Long courseId1;
+    private Long courseId2;
+}
